3e2b9f520374c1aa81e982004f23dad7c4a05d58
EmilDino <EmilLD123@hotmail.com>
2018-11-13 14:10:03 +0100

    try catch

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,33 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        boolean continueInput = true;
         int x = 0;
         int y = 0;
+        int tal = 0;
         int[] intArrray = new int[5];
         boolean dinMor = false;
+        Scanner s = new Scanner(System.in);
+        System.out.println("intast en integer");
+
+        do {
+            try {
+                tal = s.nextInt();
+                continueInput = false;
+            } catch (InputMismatchException ex) {
+                System.out.println("invalid input. you must enter an integer");
+                s.nextLine();
+            }
+            /** finally {
+                tal = 0;
+            } */
+        }while(continueInput);
+
+        System.out.println("tallet er" + tal);
+
 
         for(int i = 0; i < intArrray.length; i++) {
             intArrray[i] = i;
@@ -28,6 +51,15 @@ public class Main {
         }
         else System.out.println("Martins Mor!");
 
+        if (tal < 10) {
+            System.out.println("tallet er mindre end 10");
+        }
+        else if (tal < 20 && tal >= 10) {
+            System.out.println("tallet er mindre end 20");
+        }
+        else {
+            System.out.println("tallet er større end 20");
+        }
 
     }
 }
